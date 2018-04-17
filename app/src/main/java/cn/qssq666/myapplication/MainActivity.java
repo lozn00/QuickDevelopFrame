43aa59e001a3d3d4dbfb89a2b1ab41c36f6b3d3f
QSSQ <694886526@qq.com>
2018-04-17 09:38:04 +0800

    存档使用继承

--- app/src/main/java/cn/qssq666/myapplication/MainActivity.java
+++ app/src/main/java/cn/qssq666/myapplication/MainActivity.java
@@ -3,11 +3,16 @@ package cn.qssq666.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import cn.qssq666.rapiddevelopframe.global.SuperAppContext;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        MyApp.getInstance()
+        SuperAppContext.showToast("xxxx");
     }
+
 }
---
+++ app/src/main/java/cn/qssq666/myapplication/MyApp.java
@@ -0,0 +1,10 @@
+package cn.qssq666.myapplication;
+
+import cn.qssq666.rapiddevelopframe.global.SuperAppContext;
+
+/**
+ * Created by qssq on 2018/4/17 qssq666@foxmail.com
+ */
+
+public class MyApp extends SuperAppContext {
+}
